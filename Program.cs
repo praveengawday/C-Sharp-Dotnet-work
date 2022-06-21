741853955ee2373883a142bb323191261176ad73
praveengawday <75113567+praveengawday@users.noreply.github.com>
2022-06-21 09:21:57 +0530

    Add files via upload
    2

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _2day
+{
+    static class user
+    {
+        public static void disp()
+        {
+            Console.WriteLine("hello");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            user.disp();
+        }
+    }
+}
